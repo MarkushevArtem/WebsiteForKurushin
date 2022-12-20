c29a2d96dc421795e0f719f19834eed79238ec13
Михаил Мальцев <70252426+maltsevmihail-ris-21-1b@users.noreply.github.com>
2022-12-20 21:08:09 +0500

    Update signUp.php

--- сайт/signUp.php
+++ сайт/signUp.php
@@ -2,34 +2,44 @@
 
 <html>
     <head>
+        <link rel="stylesheet" href="styles.css">
         <style>
-  .rt {
-    background-color: #92a1cf; 
-    color: #fff;  
-    width: 340px;
-    height: 45px;
-    font:15pt Helvetica, sans-serif;
-    border-radius: 1.3rem;
-    text-decoration: none;
-  }
-  h1
+            .custom-btn,.btn-3
+{
+width: 230px;
+  height: 40px;
+}
+  body
   {
-    color: green;
+    background-image: url('assets/images/intri_m_video_dark.jpg');
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-size: 100% 100%;
   }
+input
+  {
+   border: 5px solid #cccccc;
+   border-radius: 3px;
+   outline: none; 
+   height: 35px; 
+   width: 240px;
+   font-size: 20px; 
+   font-family: Tahoma; 
+}
 </style>
         <meta charset="utf-8">
         <title>Регистрация</title>
     </head>
-    <body style='background-color: #EECFA1;'>
+    <body >
         <h1 align="center">Регистрация пользователя</h1>
         <form action="new_user.php" method="POST">
-            <p align="center"><input class = rt type="text" name="login" placeholder="Введите логин" size="28" /></p>
-            <p align="center"><input class = rt type="password" name="password1" placeholder="Введите пароль" size="28" /></p>
-            <p align="center"><input class = rt type="password" name="password2" placeholder="Пароль еще раз" size="28" /></p>
-            <p align="center"><button class = rt type="submit">Зарегистрироваться</button></p>            
+            <p align="center"><input type="text" name="login" placeholder="Введите логин" size="28" /></p>
+            <p align="center"><input  type="password" name="password1" placeholder="Введите пароль" size="28" /></p>
+            <p align="center"><input  type="password" name="password2" placeholder="Пароль еще раз" size="28" /></p>
+            <p align="center"><button class="custom-btn btn-3" type="submit">Зарегистрироваться</button></p>            
         </form>
         <form action="index.html">
-            <p align="center"><button class = rt type="submit">Вернуться на главную страницу</button></p>
+            <p align="center"><button class="custom-btn btn-3" type="submit">Вернуться на главную страницу</button></p>
         </form>
     </body>
 </html>
